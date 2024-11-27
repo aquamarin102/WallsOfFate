b10f146109fa5cedd7756cb87984aef94260bd06
Dobruy_Pez <138577710+DobruyPez@users.noreply.github.com>
2024-11-28 02:44:28 +0300

    my 1st commit

---
+++ Assets/Scripts/PlayerMuvingSystem.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMuvingSystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("hie from Stas developer");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
