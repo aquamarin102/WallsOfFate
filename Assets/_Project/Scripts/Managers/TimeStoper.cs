fa4f6342fcd6c3ba15e228a6d41db93537880c20
Dobruy_Pez <138577710+DobruyPez@users.noreply.github.com>
2025-04-07 11:09:52 +0300

    still fixing bug with inventory and pause

---
+++ Assets/_Project/Scripts/Managers/TimeStoper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets._Project.Scripts.Managers
+{
+    public class TimeStoper : MonoBehaviour
+    {
+        [field: SerializeField] public List<GameObject> TargetObjects { get; private set; }
+        private void Update()
+        {
+            if (TargetObjects != null && TargetObjects.Any(obj => obj.activeSelf != false)) TimeStop(true);
+            else TimeStop(false);
+        }
+        public void TimeStop(bool state)
+        {
+            if (state)
+            {
+                Time.timeScale = 0f; // Останавливаем время
+                Debug.Log("Игра на паузе");
+            }
+            else
+            {
+                Time.timeScale = 1f; // Восстанавливаем время
+                Debug.Log("Игра продолжается");
+            }
+        }
+    }
+}
