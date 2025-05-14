c6232441cf61372f860fbe7a13a08ebc3771b586
aquamarin102 <aquamarin120@gmail.com>
2025-05-14 20:52:23 +0300

    Boxes

--- Assets/_Project/Scripts/InteractableItems/BoxGripPoints.cs
+++ Assets/_Project/Scripts/InteractableItems/BoxGripPoints.cs
@@ -5,39 +5,4 @@ public class BoxGripPoints : MonoBehaviour
 {
     [Tooltip("Front, Back, Left, Right")]
     public Transform[] points = new Transform[4];
-
-#if UNITY_EDITOR
-    // Автогенерация для удобства (Editor только для примера)
-    [ContextMenu("Auto-create points")]
-    private void AutoCreate()
-    {
-        // если уже назначены — не трогаем
-        if (points[0]) return;
-
-        var box = GetComponent<Collider>() as BoxCollider;
-        if (!box) { Debug.LogWarning("BoxCollider not found"); return; }
-
-        Vector3 e = box.size * 0.5f;
-
-        Vector3[] localPos =
-        {
-            new Vector3(0,          0, +e.z),   // Front
-            new Vector3(0,          0, -e.z),   // Back
-            new Vector3(-e.x,       0, 0),      // Left
-            new Vector3(+e.x,       0, 0)       // Right
-        };
-
-        string[] names = { "Front", "Back", "Left", "Right" };
-
-        points = new Transform[4];
-
-        for (int i = 0; i < 4; ++i)
-        {
-            var go = new GameObject(names[i]);
-            go.transform.SetParent(transform, false);
-            go.transform.localPosition = localPos[i];
-            points[i] = go.transform;
-        }
-    }
-#endif
 }
